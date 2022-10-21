b74b01303c5bad7c017714bdeb8a23428a93848f
Your Name <you@example.com>
2022-10-21 15:44:21 +1300

    ui changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { FormEventHandler, forwardRef } from 'react';
 
 import { Link } from 'react-router-dom';
 
+
 type ButtonProps = {
     variant?: String
-    color: String
+    color?: String
     children: String
-    href: string
+    href?: string
 }
 
 const Button = (props: ButtonProps) => {
@@ -14,31 +15,17 @@ const Button = (props: ButtonProps) => {
 
     const defaultStyle: Object = {
         color: color,
-        backgroundColor: '#f5f5f5',
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: "center",
-        padding: '12px',
-        borderRadius: '2px',
+        backgroundColor: '#f5f5f5'
     }
     const outlinedStyle: Object = {
         color: color,
         backgroundColor: 'auto',
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: "center",
-        padding: '12px',
-        border: `1px solid ${color}`,
-        borderRadius: '2px',
+        border: `1px solid ${color}`
     }
     const containedStyle: Object = {
-        color: '#f5f5f5',
-        backgroundColor: color,
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: "center",
-        padding: '12px',
-        borderRadius: '2px',
+        color: '#fff',
+        backgroundColor: color === undefined ? '#156999' : color,
+        boxShadow: '0 0 0.2rem rgba(0, 0, 0, 0.3)'
     }
 
     switch (variant) {
@@ -57,19 +44,46 @@ export default Button
 type DefaultProps = {
     href: string
     children: String
-    color: String
+    color?: String
     style: Object
 }
 
 const DefaultButton = (props: DefaultProps) => {
     const { href, children, style } = props;
 
+    const sx: Object = {
+        ...style,
+        display: 'inline-block',
+        position: 'relative',
+        justifyContent: "center",
+        alignItems: "center",
+        transition: 'background 400ms',
+        padding: '12px',
+        borderRadius: '2px',
+        fontFamily: 'Roboto, sans-serif',
+    }
+
     return (
-        <Link to={href}>
-            <div
-                style={style}>
+        <ActionLink to={href}>
+            <div style={sx}>
                 {children}
             </div>
-        </Link>
+        </ActionLink>
     )
 }
+
+type ActionProps = {
+    to: string
+    children: React.ReactNode | any
+}
+
+const ActionLink = forwardRef((props: ActionProps, ref: FormEventHandler) => {
+    const { to } = props;
+    if (!to.startsWith("/")) {
+        return <a ref={ref} target="_blank" rel="noopener" href={to} {...props} />;
+    }
+    if (to.includes("#")) {
+        return <a {...props} />;
+    }
+    return <Link ref={ref} {...props} />;
+});
--- src/pages/$.tsx
+++ src/pages/$.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
+import Button from '../components/Button';
+
+const size: String = '100%'
+
+const divStyle: Object = {
+    display: 'flex',
+    justifyContent: "center",
+    alignItems: "center",
+    width: size,
+    height: size
+}
+
 export const Home = () => {
     return (
-        <></>
+        <div style={divStyle}>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: "center", alignItems: "center" }}>
+                <h1>Welcome to ticc's documentation</h1>
+                <div style={divStyle}>
+                    <Button variant="contained" href={"/architecture"}>Get started</Button>
+                    <Button variant="outlined" color={"#156999"} href={"https://github.com/ticctech"}>Open Github</Button>
+                </div>
+            </div>
+        </div >
     );
 }
 
